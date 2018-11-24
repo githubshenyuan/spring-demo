f549ff11b59414c90fca1cd8cf697ae0247b9699
¹þà½ßç <3108065@qq.com>
2018-11-24 20:45:41 +0800

    spring

--- spring-spring-demo/src/main/java/com/example/controller/DemoController.java
+++ spring-spring-demo/src/main/java/com/example/controller/DemoController.java
@@ -5,12 +5,32 @@ package com.example.controller;
  * @version ideaIU-2018.2.3.win
  */
 public class DemoController {
-    public void demo() {
+    private String username;
+    private String password;
+    public void demo( ) {
         System.out.println("demoController 输出");
+
 
     }
     public void demo(String a ,int b, boolean c) {
         System.out.println("demoController输入：" + a + "  " + "  " + b + "  " + c);
 
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
